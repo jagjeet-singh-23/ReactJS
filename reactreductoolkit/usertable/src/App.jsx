598468ce7be61dda6a87cfcccecf80d9ca2024d5
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-03-19 15:54:40 +0530

    modified:   reactreductoolkit/usertable/src/App.jsx 	new file:   reactreductoolkit/usertable/src/components/DeleteAllUsers.jsx 	new file:   reactreductoolkit/usertable/src/components/UserDetails.jsx 	modified:   reactreductoolkit/usertable/src/index.css 	new file:   reactreductoolkit/usertable/src/store/index.jsx 	new file:   reactreductoolkit/usertable/src/store/slices/UserSlice.jsx

--- reactreductoolkit/usertable/src/App.jsx
+++ reactreductoolkit/usertable/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/NavBar'
+import UserDetails from './components/UserDetails'
 
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <UserDetails />
+    </>
   )
 }
 
---
+++ reactreductoolkit/usertable/src/components/DeleteAllUsers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeleteAllUsers = () => {
+  return (
+    <div>
+      Delete All Users
+    </div>
+  )
+}
+
+export default DeleteAllUsers
