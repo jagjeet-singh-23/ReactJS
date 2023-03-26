e8c7c1fa2d15c460d739dcaa115287abc70567cb
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-03-26 14:16:49 +0530

    modified:   bank_website/bank_modern_app/src/App.jsx

--- bank_website/bank_modern_app/src/App.jsx
+++ bank_website/bank_modern_app/src/App.jsx
@@ -1,31 +1,41 @@
 import React from 'react'
 import styles from './style'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Stats from './components/Stats'
+import Business from './components/Business'
+import Billing from './components/Billing'
+import CardDeal from './components/CardDeal'
+import Testimonials from './components/Testimonials'
+import Clients from './components/Clients'
+import CTA from './components/CTA'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Bussiness
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
     </div>
