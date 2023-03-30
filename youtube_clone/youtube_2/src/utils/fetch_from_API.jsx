bba8db87805f7d0fb8b33c822dc70016112f643c
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-03-30 17:21:32 +0530

    modified:   youtube_clone/youtube_2/src/components/Feed.jsx 	modified:   youtube_clone/youtube_2/src/components/Sidebar.jsx 	modified:   youtube_clone/youtube_2/src/utils/fetch_from_API.jsx

--- youtube_clone/youtube_2/src/utils/fetch_from_API.jsx
+++ youtube_clone/youtube_2/src/utils/fetch_from_API.jsx
@@ -1,15 +1,14 @@
 import axios from 'axios'
-const Base_URL = 'https://youtube-v31.p.rapidapi.com/captions'
-
+const Base_URL = 'https://youtube-v31.p.rapidapi.com'
 const options = {
-    params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
-    headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-    }
-  };
+  params: { part: 'snippet', videoId: 'M7FIvfx5J10' },
+  headers: {
+    'X-RapidAPI-Key': '3e2d8f8508mshd51caaa05647043p177c87jsna86161c94153',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
 
-export const fetchFromAPI = async(url) => {
-    const {data} = await axios.get(`${Base_URL}/${url}`, options);
-    return data;
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${Base_URL}/${url}`, options);
+  return data;
 }
