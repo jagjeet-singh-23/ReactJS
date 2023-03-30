d4827cbad657dbd0e4bd5a192a60873468cd8447
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-03-30 19:13:28 +0530

    new file:   youtube_clone/youtube_2/src/components/ChannelCard.jsx 	modified:   youtube_clone/youtube_2/src/components/Feed.jsx 	new file:   youtube_clone/youtube_2/src/components/VideoCard.jsx 	modified:   youtube_clone/youtube_2/src/components/Videos.jsx 	modified:   youtube_clone/youtube_2/src/components/index.js

--- youtube_clone/youtube_2/src/components/Feed.jsx
+++ youtube_clone/youtube_2/src/components/Feed.jsx
@@ -33,7 +33,7 @@ const Feed = () => {
         <Typography variant='h4' fontWeight='bold' md={2} sx={{ color: "white" }}>
           {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
         </Typography>
-        <Videos video={videos} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   )
--- youtube_clone/youtube_2/src/components/Videos.jsx
+++ youtube_clone/youtube_2/src/components/Videos.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import { Stack, Box } from "@mui/material"
+import ChannelCard from "./ChannelCard"
+import VideoCard from "./VideoCard"
 
-const Videos = ({video}) => {
-  console.log(video);
-  return (
-    <div>Videos</div>
+
+const Videos = ({videos}) => (
+  <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+    {videos.map((item, ind) => {
+      <Box key={ind}>
+        {item.id.videoId && <VideoCard video={item}/>}
+        {item.id.channelId && <ChannelCard channelDetail={item}/>}
+      </Box>
+    })}
+    </Stack>
   )
-}
-
 export default Videos
