581b5020491c34ff591724e8bdbb231207f5dad5
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-03-30 12:33:31 +0530

    modified:   youtube_clone/youtube_2/src/App.jsx 	new file:   youtube_clone/youtube_2/src/utils/constants.js

--- youtube_clone/youtube_2/src/App.jsx
+++ youtube_clone/youtube_2/src/App.jsx
@@ -10,13 +10,13 @@ import {
 
 const App = () => (
   <BrowserRouter>
-    <Box sx={{backgroundColor: '#000'}}>
+    <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Feed />}/>
-        <Route path='/video/:id' element={<VideoDetail />}/>
-        <Route path='/channel/:id' element={<ChannelDetail />}/>
-        <Route path='/search/:searchTerm' element={<SearchFeed />}/>
+        <Route path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
