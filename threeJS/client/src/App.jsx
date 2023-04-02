5e9c56a6c9353a45c9807de729a1071cf1eedbde
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-04-02 22:19:51 +0530

    modified:   threeJS/client/src/App.jsx 	new file:   threeJS/client/src/canvas/index.jsx 	new file:   threeJS/client/src/components/CustomButton.jsx 	new file:   threeJS/client/src/pages/Customizer.jsx 	new file:   threeJS/client/src/pages/Home.jsx 	new file:   threeJS/client/src/store/index.js

--- threeJS/client/src/App.jsx
+++ threeJS/client/src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Canvas from "./canvas"
+import Customizer from "./pages/Customizer"
+import Home from "./pages/Home"
 
 const App = () => {
   return (
-    <div className='head-text'>App</div>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   )
 }
 
