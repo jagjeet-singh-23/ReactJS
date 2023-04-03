f1f6ebb675347183150d8cadd277432a97f77187
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-04-03 20:42:37 +0530

    modified:   threeJS/client/src/App.jsx 	new file:   threeJS/client/src/canvas/BackDrop.jsx 	new file:   threeJS/client/src/canvas/CameraRig.jsx 	new file:   threeJS/client/src/canvas/Shirt.jsx 	modified:   threeJS/client/src/canvas/index.jsx

--- threeJS/client/src/canvas/index.jsx
+++ threeJS/client/src/canvas/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei'
+import Shirt from './Shirt'
+import CameraRig from './CameraRig'
+import BackDrop from './BackDrop'
 
-const Canvas = () => {
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
+      {/* <CameraRig> */}
+        {/* <BackDrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      {/* </CameraRig> */}
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
