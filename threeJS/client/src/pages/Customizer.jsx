5355b95aabf17bf1a38f1842d9662aadb28950cc
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-04-04 19:18:23 +0530

    modified:   threeJS/client/src/pages/Customizer.jsx 	modified:   threeJS/server/.gitignore 	modified:   threeJS/server/routes/dalle.routes.js

--- threeJS/client/src/pages/Customizer.jsx
+++ threeJS/client/src/pages/Customizer.jsx
@@ -40,13 +40,13 @@ const Customizer = () => {
     if (!prompt) return alert("Please enter a prompt");
     try {
       setGeneratingImg(true);
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('http://127.0.0.1:8080/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          prompt
+          prompt,
         })
       })
       const data = await response.json();
--- threeJS/server/routes/dalle.routes.js
+++ threeJS/server/routes/dalle.routes.js
@@ -22,7 +22,7 @@ router.route('/').post(async (req, res) => {
             prompt,
             n: 1,
             size: '1024x1024',
-            response_format: 'b64_json'
+            response_format: 'b64_json',
         });
         const image = response.data.data[0].b64_json;
         res.status(200).json({ photo: image });
