2c7d40fe090b0e7085b157802f2e9f7c9f6b5618
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-04-16 20:20:38 +0530

    modified:   dalle_clone/client/src/pages/Home.jsx

--- dalle_clone/client/src/pages/Home.jsx
+++ dalle_clone/client/src/pages/Home.jsx
@@ -15,6 +15,8 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState('');
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
   useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
@@ -36,7 +38,17 @@ const Home = () => {
       }
     }
     fetchPosts();
-  }, [])
+  }, []);
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  }
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -47,16 +59,23 @@ const Home = () => {
         </p>
       </div>
       <div className="mt-16">
-        <FormField />
+        <FormField
+          labelName='Search Posts'
+          type='text'
+          name='text'
+          placeholder='Search Posts'
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
       <div className="mt-10">
         {loading ? (
           <div className="flex justify-center items-center"><Loader /></div>
         ) : <>{searchText && (
-          <h2 className="font-medium text-[#666e75] text-xl mb-3">Showing results for<span className="text-[#222328]">{searchText}</span></h2>
+          <h2 className="font-medium text-[#666e75] text-xl mb-3">Showing results for <span className="text-[#222328]">{searchText}</span></h2>
         )}
           <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
-            {searchText ? (<RenderCards data={[]} title='No Search Results Found!' />) : (<RenderCards data={allPosts} title='No Posts Found!' />)}
+            {searchText ? (<RenderCards data={searchedResults} title='No Search Results Found!' />) : (<RenderCards data={allPosts} title='No Posts Found!' />)}
           </div>
         </>}
       </div>
