04ef28f2a902a1f46c10f8b3679e0a5b936d4d74
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-04-16 07:49:15 +0530

    modified:   dalle_clone/client/src/components/FormField.jsx 	modified:   dalle_clone/client/src/pages/CreatePost.jsx 	new file:   dalle_clone/client/src/utils/index.js

--- dalle_clone/client/src/pages/CreatePost.jsx
+++ dalle_clone/client/src/pages/CreatePost.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { preview } from '../assets'
+import { getRandomPrompt } from '../utils'
+import { FormField, Loader } from '../components'
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = () => {
+
+  }
+  const handleChange = (e) => {
+
+  }
+  const handleSurpriseMe = () => {
+
+  }
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+          Create a collection of imaginative and visually stunning images using the DALL-E AI and share them with the community.
+        </p>
+      </div>
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField
+            labelName='Your Name'
+            type='text'
+            name='name'
+            placeholder='Jagjeet Singh'
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName='Prompt'
+            type='text'
+            name='prompt'
+            placeholder='A BBQ that is alive, in the style of a Pixar animated movie'
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
 
