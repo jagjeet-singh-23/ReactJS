7bb4fca9eaa6e0722b566c2e7a877b3cd277a134
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-04-29 12:48:18 +0530

    modified:   AI_Summarizer/src/services/article.js

--- AI_Summarizer/src/services/article.js
+++ AI_Summarizer/src/services/article.js
@@ -2,6 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const articleAPI = createApi({
      reducerPath: 'articleAPI',
+     baseQuery: fetchBaseQuery({
+          baseUrl: '',
+     }),
      endpoints: (builder) => ({
           getSummary: builder.query({
                query: (params) => `test`
