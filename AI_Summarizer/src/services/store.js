3dc3e88d9ec3d73586fff7369e34423ce90133f8
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-05-01 19:53:50 +0530

    Delete AI_Summarizer directory

--- AI_Summarizer/src/services/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { articleAPI } from './article';
-
-export const store = configureStore({
-    reducer: {
-        [articleAPI.reducerPath]: articleAPI.reducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleAPI.middleware)
-})
