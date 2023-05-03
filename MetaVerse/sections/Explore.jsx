1bed8b41ba57e129e87bbe83965918b551817e94
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-05-03 19:10:24 +0530

    modified:   MetaVerse/components/CustomTexts.jsx 	modified:   MetaVerse/sections/Explore.jsx

--- MetaVerse/sections/Explore.jsx
+++ MetaVerse/sections/Explore.jsx
@@ -1,8 +1,24 @@
 'use client';
 
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { staggerContainer } from '../utils/motion';
+import styles from '../styles';
+import { exploreWorlds } from '../constants';
+import { ExploreCard, TitleText, TypingText } from '../components';
+
 const Explore = () => (
-  <section>
-    Explore section
+  <section className={`${styles.paddings}`} id="explore">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| About Worlds" textStyles="text-center" />
+      <TitleText title={<>Choose the world to want <br className="md:block hidden" /> to explore.</>} textStyles="text-center" />
+    </motion.div>
   </section>
 );
 
