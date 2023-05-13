64ebe103060c7b6628e1e15ada633334eb04be1e
jagjeet-singh-23 <jagjeetsingh7967@gmail.com>
2023-05-13 09:15:22 +0530

    Made changes in InsightCard

--- MetaVerse/components/InsightCard.jsx
+++ MetaVerse/components/InsightCard.jsx
@@ -1,9 +1,12 @@
 'use client';
 
-const InsightCard = () => (
-  <div>
+import { motion } from 'framer-motion';
+import { fadeIn } from '../utils/motion';
+
+const InsightCard = ({imgUrl, title, subtitle, index}) => (
+  <motion.div>
     Insight Card
-  </div>
+  </motion.div>
 );
 
 export default InsightCard;
